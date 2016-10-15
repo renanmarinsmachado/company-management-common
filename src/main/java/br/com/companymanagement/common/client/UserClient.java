8fedff406a3b4aac9a19ffa7d021f1c7e89f7785
renan machado <renan machado>
2016-10-15 10:31:49 -0300

    Corret project structure

---
+++ src/main/java/br/com/companymanagement/common/client/UserClient.java
@@ -0,0 +1,25 @@
+package br.com.companymanagement.common.client;
+
+import static br.com.companymanagement.common.fixed.UrlUtils.GET_USER;
+import static br.com.companymanagement.common.fixed.UrlUtils.POST_USER_FILTER;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.companymanagement.common.dto.UserDTO;
+
+@Service
+public class UserClient {
+
+	@SuppressWarnings("unchecked")
+	public List<UserDTO> getUsers(){
+		RestTemplate restTemplate = new RestTemplate();
+		return (List<UserDTO>) restTemplate.getForObject(GET_USER, UserDTO.class);
+	}
+	
+	public UserDTO getUserByUsername(String username){
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.postForObject("", new UserDTO(POST_USER_FILTER, username, ""), UserDTO.class);
+	}
+}
---
+++ src/main/java/br/com/companymanagement/common/fixed/UrlUtils.java
@@ -0,0 +1,7 @@
+package br.com.companymanagement.common.fixed;
+
+public interface UrlUtils {
+	
+	public static final String GET_USER = "http://localhost:8082/ed/user";
+	public static final String POST_USER_FILTER = "http://localhost:8082/ed/user/filter";
+}
